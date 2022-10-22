26cbfedae64f7d7c99daebb52384caaeba004db2
huuhiep2505 <48766272+huuhiep2505@users.noreply.github.com>
2022-10-22 16:41:33 +0700

    update

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
  
 @app.route("/hello", methods=["GET"])
 def hello():
-    return $'Hello World. My name is Nguyen Huu Hiep'
+    return 'Hello World. My name is Nguyen Huu Hiep'
  
 app.run(host="0.0.0.0", port=80, debug=True)
